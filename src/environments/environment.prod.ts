2d891def3719516f7d0ba0bed3e1818835b41618
KARL IVAN REMONDE <karlivan.remonde.iics@ust.edu.ph>
2020-08-19 10:24:26 +0800

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBrf2MS9hWadULL3f2xyAU4J_sTum6Dz3s",
+    authDomain: "simple-web-app-547e1.firebaseapp.com",
+    databaseURL: "https://simple-web-app-547e1.firebaseio.com",
+    projectId: "simple-web-app-547e1",
+    storageBucket: "simple-web-app-547e1.appspot.com",
+    messagingSenderId: "1075484196064",
+    appId: "1:1075484196064:web:12e6565b6698537024a485",
+    measurementId: "G-Y3B8SJZ3PB"
+}
 };
